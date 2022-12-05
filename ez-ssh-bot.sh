b91677a57259c2e9ca797bf739bb408b3e249fb1
Stefan Gränitz <stefan.graenitz@gmail.com>
2022-12-05 20:07:16 +0100

    Add IPv6 case for local loopback in

--- ez-ssh-bot.sh
+++ ez-ssh-bot.sh
@@ -3,7 +3,7 @@
 # We are subscribed to session events and ignore closing ones
 if [ "$PAM_TYPE" != "close_session" ]; then
   # Only monitor reverse-tunnel connections (they come in via local loopback)
-  if [ "$PAM_RHOST" == "127.0.0.1" ]; then
+  if [[ "$PAM_RHOST" == "127.0.0.1" || "$PAM_RHOST" == "::1" ]]; then
     CHAT_ID=<your channel ID>
     BOT_TOKEN=<your bot-token>
     message="$(date +"%Y-%m-%d, %A %R")"$'\n'"External SSH Login: $PAM_USER@$(hostname)"
